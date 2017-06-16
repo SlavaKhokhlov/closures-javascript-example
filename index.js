054c214a2a441142275af9e75f9568f10e74607e
Slava Khokhlov <slava.khokhlov@gmail.com>
2017-06-17 06:58:02 +1000

    more examples

--- index.js
+++ index.js
@@ -7,3 +7,44 @@ count();
 count();
 count();
 // the counter is now 3
+
+// ----------------------------------------------
+
+function greet(whattosay) {
+
+	return function(name) {
+		console.log(whattosay + ' ' + name);
+	}
+	
+}
+
+//greet('Hi')('Slava');
+
+var sayHi = greet('Hi');
+sayHi('Slava');
+
+//----------------------------------------------------
+
+function buildFunctions2() {
+ 
+    var arr = [];
+    
+    for (var i = 0; i < 3; i++) {
+        arr.push(
+            (function(j) {
+                return function() {
+                    console.log(j);   
+                }
+            }(i))
+        )
+        
+    }
+    
+    return arr;
+}
+
+var fs2 = buildFunctions2();
+
+fs2[0]();
+fs2[1]();
+fs2[2]();
